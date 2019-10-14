00942792dac02f9c18f1003f15a624913b5611c0
Tylor Schafer <tylorschafer@gmail.com>
2019-10-14 10:30:55 -0600

    Add phoenix_member_service

--- app/facades/phoneix_members_search.rb
+++ app/facades/phoneix_members_search.rb
@@ -8,19 +8,8 @@ class PhoneixMembersSearch
   end
 
   def characters
-    conn = Faraday.new(url: 'https://www.potterapi.com/v1/characters') do |f|
-      f.adapter Faraday.default_adapter
-      f.params['key'] = ENV['POTTER_API_KEY']
-    end
-
-    response = conn.get do |req|
-      req.params['house'] = house
-      req.params['orderOfThePhoenix'] = true
-    end
-
-    character_data = JSON.parse(response.body, symbolize_names: true)
-
-    character_data.map do |char_data|
+    service = PhoenixMembersService.new
+    service.characters_by_house(house).map do |char_data|
       Character.new(char_data)
     end
   end
--- app/services/phoenix_members_service.rb
+++ app/services/phoenix_members_service.rb
@@ -1,3 +1,23 @@
 class PhoenixMembersService
 
+  def characters_by_house(house)
+    get_json("/characters", house)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://www.potterapi.com/v1/characters') do |f|
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url, house)
+    response = conn.get do |req|
+      req.params['key'] = ENV['POTTER_API_KEY']
+      req.params['house'] = house
+      req.params['orderOfThePhoenix'] = true
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
